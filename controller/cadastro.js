34d72d48a6a66c80b912442a83e6a74d65e51365
Heder Alves <83372052+HederAlves@users.noreply.github.com>
2021-11-07 23:27:28 -0300

    Upload
    Cadastro de produtos

---
+++ controller/cadastro.js
@@ -0,0 +1,20 @@
+import { view } from "../view/index.js"
+
+export const CadastroComponet = () => {
+    view.getcadastroHtml();
+    
+    const formulario = document.getElementById("formulario")
+    formulario.addEventListener('submit', function(event) {
+        event.preventDefault()
+        
+        const cadastroProdutos = {
+            name: document.getElementById('name').value,
+            mark: document.getElementById('marca').value,
+            quantity: document.getElementById('quantidade').value,
+            type: document.getElementById('tipo').value,
+            code: document.getElementById('codigo').value,
+            observation: document.getElementById('observacoes').value
+        }
+       service.post(cadastroProdutos)
+    })
+}
