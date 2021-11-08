e4b6ddadd3724314041222d1abc83170038c6bbe
Heder Alves <83372052+HederAlves@users.noreply.github.com>
2021-11-07 23:35:34 -0300

    Update 1.0
    Inserindo lista de produtos ao projeto.

--- view/index.js
+++ view/index.js
@@ -1,6 +1,6 @@
-
 export const view = {
     getcadastroHtml,
+    getListaProdutosHtml
 }
 
 const getcadastroHtml = () => {
@@ -12,6 +12,9 @@ const getcadastroHtml = () => {
     <br><br>
     <label>Marca</label>
     <input id="marca" type="text" placeholder="Digite a marca do produto">
+    <br><br>
+    <label>Quantidade</label>
+    <input id="quantidade" type="number">
     <br><br>
     <select id="tipo" type="text" placeholder="Digite o tipo do produto">
         <option value="1">Perecível</option>
@@ -31,4 +34,27 @@ const getcadastroHtml = () => {
     </form>`
 
     main.innerHTML = dadosHtml
+}
+
+const getListaProdutosHtml = () => {
+    const main = document.getElementById('root')
+    const dadosHtml = `
+        <section>
+            <h1>Lista de Clientes</h1>
+            <table id="tbody">
+            <tr>
+                <th>Nome</th>
+                <th>Marca</th>
+                <th>Quantidade</th>
+                <th>Tipo</th>
+                <th>Código</th>
+                <th>Observações</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+                <th>Novo</th>
+            </tr>
+            </table>
+        </section> `
+
+        main.innerHTML = dadosHtml
 }
