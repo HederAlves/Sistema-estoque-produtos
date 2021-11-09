3f6e06a45c121ec8cadf433dc3b2a16da1fad4cb
Heder Alves <83372052+HederAlves@users.noreply.github.com>
2021-11-09 18:02:56 -0300

    Update 2.0
    Adicionando HTML para atualizar produtos.
    Projeto finalizado.

--- view/index.js
+++ view/index.js
@@ -1,8 +1,62 @@
 
 const getcadastroHtml = () => {
     const main = document.getElementById('root')
-    const dadosHtml = `<form id="formulario">
+    const dadosHtml = `<form class="formulario" id="formulario">
     <h1>Novo Produto</h1>
+    <label>Produto</label>
+    <input id="name" type="text" placeholder="Digite o nome do produto">
+    <br><br>
+    <label>Marca</label>
+    <input id="marca" type="text" placeholder="Digite a marca do produto">
+    <br><br>
+    <label>Quantidade</label>
+    <input id="quantidade" type="number" placeholder="Digite a quantidade do produto">
+    <br><br>
+    <label>Tipo</label>
+    <select id="tipo" class="tipo"  placeholder="Digite o tipo do produto">
+        <option value="1" >Perecível</option>
+        <option value="2" >Objeto</option>
+    </select>
+    <br><br>
+    <label>Código</label>
+    <input id="codigo" type="text" placeholder="Digite o código do produto">
+    <br><br>
+    <label>Observações</label>
+    <input id="observacoes" type="text" placeholder="Digite a observação">
+    <br><br>
+    <div>
+        <button id="cancelar" onclick="reset()" type="button">Cancelar</button>
+        <button id="salvar" type="submit">Salvar</button>
+    </div>
+    </form>`
+
+    main.innerHTML = dadosHtml
+}
+
+const getListaProdutosHtml = () => {
+    const main = document.getElementById('root')
+    const dadosHtml = `
+        <section class="listaProdutos">
+            <h1>Lista de Produtos</h1>
+            <table class="tbody" id="tbody">
+            <tr>
+                <th>Nome</th>
+                <th>Marca</th>
+                <th>Quantidade</th>
+                <th class="tipo">Tipo</th>
+                <th>Código</th>
+                <th>Observações</th>
+            </tr>
+            </table>
+        </section> `
+
+        main.innerHTML = dadosHtml
+}
+
+const getAtualizaHtml = () => {
+    const main = document.getElementById('root')
+    const dadosHtml = `<form class="formulario" id="formulario">
+    <h1>Editar Produto</h1>
     <label>Produto</label>
     <input id="name" type="text" placeholder="Digite o nome do produto">
     <br><br>
@@ -24,7 +78,6 @@ const getcadastroHtml = () => {
     <input id="observacoes" type="text" placeholder="Digite a observação">
     <br><br>
     <div>
-        <button id="cancelar" type="button">Cancelar</button>
         <button id="salvar" type="submit">Salvar</button>
     </div>
     </form>`
@@ -32,27 +85,8 @@ const getcadastroHtml = () => {
     main.innerHTML = dadosHtml
 }
 
-const getListaProdutosHtml = () => {
-    const main = document.getElementById('root')
-    const dadosHtml = `
-        <section>
-            <h1>Lista de Produtos</h1>
-            <table id="tbody">
-            <tr>
-                <th>Nome</th>
-                <th>Marca</th>
-                <th>Quantidade</th>
-                <th class="tipo">Tipo</th>
-                <th>Código</th>
-                <th>Observações</th>
-            </tr>
-            </table>
-        </section> `
-
-        main.innerHTML = dadosHtml
-}
-
 export const view = {
     getcadastroHtml,
-    getListaProdutosHtml
+    getListaProdutosHtml,
+    getAtualizaHtml
 }
