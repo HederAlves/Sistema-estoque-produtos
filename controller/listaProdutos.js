26961ca15d19ed4d6b45fb2669cc49bccccd05d5
Heder Alves <83372052+HederAlves@users.noreply.github.com>
2021-11-08 21:53:52 -0300

    Update 1.0
    Melhorando código.
    Inserindo tabela e integrando frontend ao backend e banco de dados.

--- controller/listaProdutos.js
+++ controller/listaProdutos.js
@@ -1,20 +1,52 @@
+
+import { service } from "../service/index.js"
 import { view } from "../view/index.js"
 
 export const ListaProdutosComponent = () => {
     view.getListaProdutosHtml()
-    
+
+    service.getProdutos().then((dados) => {
+        dados.forEach((element) => {
+            if(element.name != null && element.code != null){
+                criarNovaLinha(element.name, element.mark, element.quantity, element.type, element.code, element.observation, element.id)
+            }
+        });
+    })
+
     const table = document.getElementById('tbody')
     table.addEventListener('click', (event)=> {
         const button = event.path[0].innerText
         
         if(button === 'Editar') {
-            console.log(button)
+        
         }
         if(button === 'Excluir') {
-            console.log(button)
+           
         }
         if(button === 'Novo') {
-            console.log(button)
+            
         }
     })
+}
+
+
+
+const criarNovaLinha = (produto, marca, quantidade, tipo, codigo, observacoes, id) => {
+    const table = document.getElementById('tbody')
+    const NovaLinha = document.createElement('tr')
+    const dadosHtml = `
+        <td class="none">${produto}</td>
+        <td>${marca}</td>
+        <td>${quantidade}</td>
+        <td class="none">${tipo}</td>
+        <td class="none">${codigo}</td>
+        <td class="none">${observacoes}</td>
+            <div class="lista-btn">
+                <a id="${id}" class="btn-link editar" >Editar</a>
+                <a id="${id}" class="btn-link type" >Excluir</a>
+            </div>        
+        </td>
+        `
+    NovaLinha.innerHTML = dadosHtml
+    return table.appendChild(NovaLinha)
 }
